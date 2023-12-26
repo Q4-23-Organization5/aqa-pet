a55926154ff93c7ece0a0ff709fc4b2b364b00ac
Slava Koshun <fogamyster@gmail.com>
2023-12-26 23:55:45 +0200

    EquilateralTriangle

---
+++ out/production/aqa-pet/HomeWork6/EquilateralTriangle.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class EquilateralTriangle {
+    public static void main(String[] args) {
+
+        //Welcome in system
+        System.out.println("Welcome in Equilateral Triangle. Enter the first side");
+
+        //Add number1
+        Scanner firstNumb = new Scanner(System.in);
+
+        //Save scanner to variable
+        int getOne = firstNumb.nextInt();
+
+        //Add number2
+        Scanner secondNumb = new Scanner(System.in);
+
+        //Save scanner to variable
+        System.out.println("Enter the second side");
+        int getSecond = firstNumb.nextInt();
+
+
+        //Add number3
+        Scanner thirdNumb = new Scanner(System.in);
+
+        //Save scanner to variable
+        System.out.println("Enter the third side");
+        int getThird = firstNumb.nextInt();
+
+        //Run checking
+        if (getThird != 0 && getOne == getSecond && getOne == getThird) {
+            System.out.println("Triangle is equilateral");
+        }  else {
+                System.out.println("Triangle is NOT equilateral");
+        }
+
+    }
+}

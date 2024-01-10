0ee915ede2e9bb05f035dd6d1cb971a632464897
SlavaKoshun <122649837+SlavaKoshun@users.noreply.github.com>
2024-01-11 00:30:56 +0200

    Merge pull request #7
    * CheckOddEven
    
    * PrintMonthInWord
    
    * AbsoluteValue
    
    * AbsoluteValue + one comment)
    
    * EquilateralTriangle
    
    * MiddleLetter
    
    * Last small fixes
    
    * Hm 6 (#2)
    
    * Merge remote-tracking branch 'origin/main'
    
    * Hm 7 (#4)
    
    * Merge remote-tracking branch 'origin/main'
    
    * Ex. 2. Class Calculator.
    
    * HM_9
    
    * HM_9
    
    * Merge remote-tracking branch 'origin/HM_9' into HM_9
    
    * HM_9

---
+++ src/homework5/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Exercise 1
+        System.out.println("Exercise 1.0");
+        System.out.println("My First Java Program");
+        System.out.println(" ");
+
+
+        //Exercise2
+        System.out.println("Exercise 2");
+
+        float celsius;
+        float fahrenheit;
+        double kelvin;
+
+        celsius = 5;
+
+        fahrenheit = celsius + 32;
+        kelvin = celsius + 273.16;
+
+        System.out.println(celsius + " Celsius: " + fahrenheit + " Fahrenheit; " + kelvin + " Kelvin");
+        System.out.println(" ");
+
+        //Exercise3
+        System.out.println("Exercise 3");
+
+        float firstSide;
+        float secondSide;
+
+        firstSide = 3;
+        secondSide = 5;
+
+        float S = (firstSide * secondSide) / 2;
+
+        System.out.println("Розрахунок площі " + S);
+    }}

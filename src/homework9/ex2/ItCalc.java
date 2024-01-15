a028adb67adf4aa4900f74dc72e4123daf53e710
SlavaKoshun <122649837+SlavaKoshun@users.noreply.github.com>
2024-01-15 11:47:57 +0200

    Hm_9 (#6)
    * Add cod and .gitignore comfig.
    
    * Add cod and .gitignore comfig.
    
    * Add cod and .gitignore comfig.
    
    * CheckOddEven
    
    * PrintMonthInWord
    
    * AbsoluteValue
    
    * AbsoluteValue + one comment)
    
    * EquilateralTriangle
    
    * MiddleLetter
    
    * Last small fixes
    
    * Ex. 2. Class Calculator.
    Ex. 1. Class User.
    
    * HM_9
    
    * HM_9
    
    * HM_9
    
    * HM_9 - improvement for calculator is added (I commented out the old version to save it for myself)
    
    ---------
    
    Co-authored-by: slavakosun <ykoshun@crmdata.info>

---
+++ src/homework9/ex2/ItCalc.java
@@ -0,0 +1,29 @@
+package ex2;
+
+public class ItCalc extends BasicCalc {
+
+/*    public ItCalc(double numberOne, double numberTwo) {
+        super(numberOne, numberTwo);
+    }
+
+    public ItCalc(double numberOne) {
+        super(numberOne);
+    }
+
+ */
+
+    public static double log(double numberOne) {
+        return Math.log(numberOne);
+    }
+
+/*    @Override
+    public String toString() {
+        return "ItCalc{" +
+                "numberOne=" + numberOne +
+                ", numberTwo=" + numberTwo +
+                '}';
+    }
+
+ */
+}
+

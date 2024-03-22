e8eb606a8e5550ec47a93192307efbefb3a8e538
Slava Koshun <fogamyster@gmail.com>
2024-03-22 18:49:34 +0200

    Page Factory and add log
    Page Factory

---
+++ src/main/java/homework19AutoRiaRefactoring/CarListPage2.java
@@ -0,0 +1,48 @@
+package homework19AutoRiaRefactoring;
+
+import homework14.loger.StartGame;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CarListPage2 extends BasePage{
+
+    final Logger logger = LogManager.getLogger(StartGame.class);
+
+    public CarListPage2(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(xpath = "//span[text()=' Y6DSF69YE8W041095 ']")
+    private WebElement vinCod;
+    @FindBy(xpath = "//span[@class='state-num ua' and text()=' AO 2475 EO ']")
+    private WebElement carNumber;
+    @FindBy(xpath = "//a[@title='Седан Chevrolet Aveo 2009 в Сваляві']")
+    private WebElement carButton;
+
+    public String findVinCod() {
+        String vinResult = "Y6DSF69YE8W041095";
+        vinCod.isDisplayed();
+        logger.debug("vinCod is found");
+        return vinResult;
+    }
+
+    public String getCarNumber() {
+        String number = "AO2475EO";
+        try{
+            carNumber.isDisplayed();
+            logger.debug("Car number is found");
+        } catch (NoSuchElementException ex) {
+            logger.debug("Is not found");
+        }
+        return number;
+    }
+
+    public void clickOnCar() {
+        carButton.click();
+        logger.debug("Car button is found");
+    }
+
+}

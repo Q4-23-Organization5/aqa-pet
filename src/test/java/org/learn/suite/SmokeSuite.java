d805ab3ab4784566ca329141b03b77c10f1fb5bc
Slava Koshun <fogamyster@gmail.com>
2024-04-03 15:41:51 +0300

    changes3 looks like good

---
+++ src/test/java/org/learn/suite/SmokeSuite.java
@@ -0,0 +1,10 @@
+package org.learn.suite;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.learn.homework19.AutoRiaRefactoring;
+
+@Suite
+@SelectClasses(AutoRiaRefactoring.class)
+public class SmokeSuite {
+}
